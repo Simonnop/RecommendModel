4c3c376a94188d38f6f78875c03e67e2834ca7c0
SuRuixian <741617293@qq.com>
2022-12-31 15:36:08 +0800

    Initial commit

---
+++ bin/db_connect.py
@@ -0,0 +1,22 @@
+import pymongo
+
+# Replace the uri string with your MongoDB deployment's connection string.
+conn_str = "mongodb://1.15.118.125:27017/"
+
+# set a 5-second connection timeout
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+
+try:
+    print(client.server_info())
+except Exception:
+    print("Unable to connect to the server.")
+
+collection_user = client.get_database("NIC").get_collection("User")
+
+for column in collection_user.find():
+    for mission_id in column.get("missionTaken"):
+        print(mission_id)
+
+
+
+
